81800c61a6bbe43b097a2a3ab15e97f510753089
ashish1patel <pashish.18mail@gmail.com>
2023-06-13 13:45:37 +0100

    homework submited

---
+++ src/homework_week_9/Programme_6_RetrieveArraylistAtGivenIndex.java
@@ -0,0 +1,31 @@
+package homework_week_9;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/*
+Write a Java program to retrieve an element (at a specified index) from a given array list
+ */
+public class Programme_6_RetrieveArraylistAtGivenIndex {
+    public static void main(String[] args) {
+        ArrayList<String> vegetableList = new ArrayList<>();
+        vegetableList.add("Cucumber");
+        vegetableList.add("Potato");
+        vegetableList.add("Cauliflower");
+        vegetableList.add("Bringle");
+        vegetableList.add("Onion");
+        vegetableList.add("Beetroot");
+        vegetableList.add("Mashroom");
+        vegetableList.add("Bitter_Gourd");
+        vegetableList.add("vegetableLst");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter an index number : ");
+        int index = scanner.nextInt();
+        if (index < vegetableList.size()) {
+            System.out.println(vegetableList.get(index));
+        }else {
+            System.out.println("Invalid index!!");
+        }
+        scanner.close();
+    }
+}
